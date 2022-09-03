b2f67993be3cfcbde8ba21a75e11903c24204874
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-09-03 13:18:04 +0300

    contries.js

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,12 @@
+import Notiflix from "notiflix";
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+export function fetchCountries(name) {
+  return (`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+      if (!response.ok) {
+        throw Notiflix.Notify.failure("Oops, there is no country with that name")
+      }
+      return response.JSON();
+    });
+}
