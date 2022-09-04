e15926ddc4bf2e188548e073ac604aa854190318
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-09-04 17:09:00 +0300

    c

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,11 +2,11 @@ import Notiflix from "notiflix";
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 export function fetchCountries(name) {
-  return (`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response => {
       if (!response.ok) {
         throw Notiflix.Notify.failure("Oops, there is no country with that name")
       }
-      return response.JSON();
+      return response.json();
     });
 }
